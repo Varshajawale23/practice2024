758ac538f3526d8c6e7d77110229f10b087384c8
Varshajawale23 <varshajawale23@gmail.com>
2024-01-01 09:44:36 +0530

    first commit

---
+++ object.py
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Object</title>
+<body>
+<h1 align="center">Object</h1>
+<hr>
+<pre>
+<b>What is object ?</b>
+     -It is a physical reality.
+     -Object is an instance of a class.   
+     -It is Consume Memory.
+     -Every object is Unique identifier by hashcode.
+     For Ex.car,bike,chair,pen. 
+ 
+<b>Syntax Of Object:</b>
+     <b>ClassName ObjectName= new ClassName();</b>
+
+     <b>ClassName:</b>  Class that we have created for creating an object.
+     <b>ObjectName:</b> Name of the object that we want to create.
+     <b>new:</b>        The new is keyword that is use to assign the memory for the object.
+     <b>ClassName():</b>It is a Constructor.
+
+<b>Characteristic of Object:</b>
+     It is three characteristic of object.
+     <b>Identity:</b>It gives a unique name to an object.for Ex.Dog
+     <b>state:</b>   It is represents by attributes of an object.for Ex.breed,Age,Color
+     <b>Behavior:</b>It is represented by the methods of an object.for Ex.Bark,sleep,eat.
+</pre>
+</body>
+<head>
+</html>
